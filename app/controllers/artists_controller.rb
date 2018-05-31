e3ffa6d3a04638f5c3d09ea1a28ddf7110263242
Michelle Olivieri <michelleo72@gmail.com>
2018-05-31 21:25:22 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,6 +23,7 @@ class ArtistsController < ApplicationController
   end
 
   def update
+    @artist = Artist.find(params[:id])
     @artist = Artist.update(artist_params(:name, :bio))
     @artist.save
     redirect_to artist_path(@artist)
