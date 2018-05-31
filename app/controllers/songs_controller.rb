50a317160e35b22e4ac12c5d10a4397c06e2b541
Michelle Olivieri <michelleo72@gmail.com>
2018-05-31 21:22:54 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song = Song.new(song_params)
     @song.save
    	redirect_to song_path(@song)
   end
